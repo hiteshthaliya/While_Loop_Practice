067113ffec6104132d2a119ab46dfc7b119de07e
Hitesh Thaliya <hiteshthaliya123@gmail.com>
2026-06-07 17:32:04 +0530

    first Commit

--- 04_odd_numbers.py
+++ 04_odd_numbers.py
@@ -2,5 +2,5 @@
 i=1
 while i<=100:
     if i%2 !=0:
-        print(i)
+      print(i)
     i +=1    
---
+++ 06_sum_n_natural_numbers.py
@@ -0,0 +1,12 @@
+ # Calculate and print the sum of the first n natural numbers
+
+n = int(input("Enter a number: "))
+
+total = 0
+i = 1
+
+while i <= n:
+    total += i
+    i += 1
+
+print(f"Sum of first {n} natural numbers is: {total}")
