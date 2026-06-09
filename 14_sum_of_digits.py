9bff4b25e3ff5a2ee44faf9b61c8184d89e644e5
Hitesh Thaliya <hiteshthaliya123@gmail.com>
2026-06-09 22:56:27 +0530

    modify print statement

--- 14_sum_of_digits.py
+++ 14_sum_of_digits.py
@@ -1,8 +1,9 @@
 #  Find and print the sum of digits of the given number.
 n = int(input("enter number: "))
+original_n = n
 total = 0
 while n>0:
     digit = n%10
     total +=digit
     n = n//10
-print("total sum of number: ",total)    
+print(f"The sum of digit of {original_n} is: {total}")
