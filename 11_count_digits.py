7269418a5087f5baa9d1005a61588fdc5a726334
Hitesh Thaliya <hiteshthaliya123@gmail.com>
2026-06-09 11:55:18 +0530

    first commit

---
+++ 11_count_digits.py
@@ -0,0 +1,10 @@
+# Count and print the total number of digits in a given number.
+n=int(input("enter number: "))
+counter = 0
+
+while n>0 :
+    n = n // 10 # floor division
+    counter += 1
+
+print(counter)        
+    
