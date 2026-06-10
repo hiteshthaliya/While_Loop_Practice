437a2c2407cbde24cd09a92f0e6dbd4abd6c0b48
Hitesh Thaliya <hiteshthaliya123@gmail.com>
2026-06-10 12:53:19 +0530

    first commit

---
+++ 17_prime_1_to_100.py
@@ -0,0 +1,15 @@
+# Print all prime numbers between 1 and 100. 
+# A prime number is a number greater than 1 and
+# that has exactly two factors: 1 and itself.
+n = 2
+while n<=100:
+    i=2
+    is_prime = True
+    while i<n:
+        if n%i == 0:
+            is_prime =False
+            break
+        i+=1
+    if is_prime:
+        print(n)
+    n+=1    
