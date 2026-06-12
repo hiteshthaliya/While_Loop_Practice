b6807a8f58863803f72f318ba5b0dfa39183200e
Hitesh Thaliya <hiteshthaliya123@gmail.com>
2026-06-12 22:47:14 +0530

    first commit

---
+++ 19_fibonacci_series.py
@@ -0,0 +1,11 @@
+#  Print the Fibonacci series up to n terms
+n = int(input("Enter the number for fibonacci series: "))
+a , b = 0 , 1
+count = 0
+
+while count<n:
+    print(a,end=" ")
+    c = a + b
+    a = b
+    b = c
+    count +=1
